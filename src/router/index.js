fbdfefdfed254fbd5ff2839bbcb0dfe7beb7580e
Kim <lkimluu02@gmail.com>
2026-07-03 23:06:15 +0800

    feat: 建立公開醫院地圖頁並串接導覽

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Home from '@/views/HomeView.vue'
 import Medical from '@/views/MedicalView.vue'
 import Dashboard from '@/views/DashboardView.vue'
 import Growth from '@/views/GrowthView.vue'
+import Hospital from '@/views/HospitalView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -64,6 +65,11 @@ const router = createRouter({
       meta: {
         requiresAuth: true,
       },
+    },
+    {
+      path: '/hospital',
+      name: 'Hospital',
+      component: Hospital,
     },
   ],
 })
